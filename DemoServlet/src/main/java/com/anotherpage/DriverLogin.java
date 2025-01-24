e4b7a65dff162836f5c167b07b7563e4a78c8f88
shivambhaskar562 <shivambhaskar562@gmail.com>
2025-01-24 22:21:33 +0530

    update

--- DemoServlet/src/main/java/com/anotherpage/DriverLogin.java
+++ DemoServlet/src/main/java/com/anotherpage/DriverLogin.java
@@ -11,7 +11,7 @@ import org.postgresql.Driver;
 public class DriverLogin {
 	private static final String url = "jdbc:postgresql://localhost:5432/ipdata?user=postgres&password=123";
 	private static final String sql = "select * from register where email = ? and fname= ? and lname=?";
-	
+
 	static {
 		try {
 			DriverManager.registerDriver(new Driver());
@@ -19,31 +19,30 @@ public class DriverLogin {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public boolean retrive(UserClass user) {
 		Connection con = null;
 		PreparedStatement pstm = null;
-		
+
 		try {
 			con = DriverManager.getConnection(url);
 			pstm = con.prepareStatement(sql);
-			
-			
-			pstm.setString(1,user.email);
-			pstm.setString(1,user.fname);
-			pstm.setString(1,user.lname);
-			
+
+			pstm.setString(1, user.email);
+			pstm.setString(1, user.fname);
+			pstm.setString(1, user.lname);
+
 			ResultSet rs = pstm.getResultSet();
-			if(rs.next()) {
+			if (rs.next()) {
 				return true;
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		finally {
 			try {
 				pstm.close();
@@ -55,4 +54,3 @@ public class DriverLogin {
 		return false;
 	}
 }
-
